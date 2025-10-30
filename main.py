ff558c9bb685e3a0a28a8ea90f2ccfb004633ddd
Minipatata <ashleynaemmalee3827@ele.cssda.ca>
2025-10-29 22:08:43 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+birthday_a="June 26th 2002"
+birthday_b="December 31st 2002"
+birthday_c="July 1st 2004"
+birthday_d="November 3d 2005"
+birthday_e="Febuary 10th 2006"
+birthday_f="December 6th 2007"
+
+
+print("My birthday is", birthday_a)
+print("My birthday is", birthday_b)
+print("My birthday is", birthday_c)
+print("My birthday is", birthday_d)
+print("My birthday is", birthday_e)
+print("My birthday is", birthday_f)
